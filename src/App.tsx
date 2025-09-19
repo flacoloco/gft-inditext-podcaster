bd6ba232f9441afac8dd363ecc6e67a90640a398
Alejandro Vásquez <alejandro@flacoloco.com>
2025-09-19 15:33:04 +0200

    fix: remove context

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Podcast, Episode } from './components/pages';
 import { Layout } from './components/templates';
-import { AppProvider } from '@src/contexts';
 import './App.css';
 import type { FC } from 'react';
 
 export const App: FC = () => {
   return (
-    <AppProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/podcast/:podcastId' element={<Podcast />} />
-            <Route path='/podcast/:podcastId/episode/:episodeId' element={<Episode />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </AppProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/podcast/:podcastId' element={<Podcast />} />
+          <Route path='/podcast/:podcastId/episode/:episodeId' element={<Episode />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
--- src/contexts/index.ts
+++
@@ -1,2 +0,0 @@
-export { AppProvider } from './AppContext';
-export { useAppContext } from './useAppContext';
