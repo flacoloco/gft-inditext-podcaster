99ecbcb5884e041eefff420e007f4a3521f65566
Alejandro Vásquez <alejandro@flacoloco.com>
2025-09-19 14:19:32 +0200

    feat: new EpisodeCard component

---
+++ src/components/pages/Episode/Episode.styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledEpisodeContainer = styled.div`
+   margin: 0;
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  padding-top: 60px;
+`;
