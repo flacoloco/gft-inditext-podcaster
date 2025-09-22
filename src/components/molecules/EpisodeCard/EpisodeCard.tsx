a918c1a9fb7852e28f3b05bae17a5e8b6bcfbc50
Alejandro Vásquez <alejandro@flacoloco.com>
2025-09-22 23:56:07 +0200

    feat: using dompurify

--- src/components/molecules/EpisodeCard/EpisodeCard.tsx
+++ src/components/molecules/EpisodeCard/EpisodeCard.tsx
@@ -1,28 +1,34 @@
 import { type FC } from 'react';
+import DOMPurify from 'dompurify';
 import {
-    StyledEpisodeCard,
-    StyledEpisodeTitle,
-    StyledEpisodeDescription,
-    StyledAudioPlayer,
+  StyledEpisodeCard,
+  StyledEpisodeTitle,
+  StyledEpisodeDescription,
+  StyledAudioPlayer,
 } from './EpisodeCard.styles';
 
 interface EpisodeCardProps {
-    title: string;
-    description: string;
-    episodeUrl: string;
+  title: string;
+  description: string;
+  episodeUrl: string;
 }
 
 export const EpisodeCard: FC<EpisodeCardProps> = ({ title, description, episodeUrl }) => {
-    return (
-        <StyledEpisodeCard>
-            <StyledEpisodeTitle>{title}</StyledEpisodeTitle>
-            <StyledEpisodeDescription>{description}</StyledEpisodeDescription>
-            <StyledAudioPlayer controls>
-                <source src={episodeUrl} type='audio/mpeg' />
-                <source src={episodeUrl} type='audio/ogg' />
-                <source src={episodeUrl} type='audio/wav' />
-                Your browser does not support the audio element.
-            </StyledAudioPlayer>
-        </StyledEpisodeCard>
-    );
+  // Sanitize the HTML content to prevent XSS attacks
+  const sanitizedDescription = DOMPurify.sanitize(description);
+
+  return (
+    <StyledEpisodeCard>
+      <StyledEpisodeTitle>{title}</StyledEpisodeTitle>
+      <StyledEpisodeDescription
+        dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
+      />
+      <StyledAudioPlayer controls>
+        <source src={episodeUrl} type='audio/mpeg' />
+        <source src={episodeUrl} type='audio/ogg' />
+        <source src={episodeUrl} type='audio/wav' />
+        Your browser does not support the audio element.
+      </StyledAudioPlayer>
+    </StyledEpisodeCard>
+  );
 };
